bd2a5b49d8da94ede435c53f24e07e321865c272
macc808 <pismenukmaksim@gmail.com>
2026-06-30 18:45:57 +0300

    view

--- blogApp/views.py
+++ blogApp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+
+def post_list(request):
+    posts = Post.objects.all().order_by('-published_date')
+    return render(request, 'post_list.html', {'posts': posts})
